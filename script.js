c164412dd9d4db6e4583a19f74f0db8bca78ed3f
ianspractice <bodybody.ian@gmail.com>
2025-01-28 11:39:47 -0600

    Add human & computer scores to playRound function

--- script.js
+++ script.js
@@ -26,19 +26,33 @@ function getHumanChoice() {
 function playRound(humanChoice, computerChoice) {
   if (humanChoice === "rock" && computerChoice === "paper") {
     alert("Computer wins! Paper covers rock.");
+    computerScore += 1;
   } else if (humanChoice === "rock" && computerChoice === "scissors") {
     alert("Human wins! Rock breaks scissors.");
+    humanScore += 1;
   } else if (humanChoice === "paper" && computerChoice === "rock") {
     alert("Human wins! Paper covers rock.");
+    humanScore += 1;
   } else if (humanChoice === "paper" && computerChoice === "scissors") {
     alert("Computer wins! Scissors cut paper.");
+    computerScore += 1;
   } else if (humanChoice === "scissors" && computerChoice === "paper") {
     alert("Human wins! Scissors cut paper.");
+    humanScore += 1;
   } else if (humanChoice === "scissors" && computerChoice === "rock") {
     alert("Computer wins! Rock breaks scissors.");
+    computerScore += 1;
   } else {
     alert("Tie!");
   }
 }
 log("Human: " + humanSelection + " Computer: " + computerSelection);
 playRound(humanSelection, computerSelection);
+log(humanScore);
+log(computerScore);
+
+//play the game 5 times
+function playGame() {}
+//update the human score and player score
+
+//declare a winner
