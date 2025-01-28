18264c2eeeed33ae810a13c4c8a74d8f0e29460f
ianspractice <bodybody.ian@gmail.com>
2025-01-28 11:16:22 -0600

    Create getComputerChoice function

---
+++ script.js
@@ -0,0 +1,11 @@
+//calculate computer's choice to randomnly select rock, paper, or scissors
+function getComputerChoice() {
+  let randomNumber = Math.random();
+  if (randomNumber < 0.26) {
+    return "rock";
+  } else if (randomNumber < 0.62) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+}
