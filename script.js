c61a4feeb1a4958be0b1a4ea7206015d1e986c4a
ianspractice <bodybody.ian@gmail.com>
2025-01-28 11:17:32 -0600

    Create variables for humanScore and computerScore

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const log = console.log;
+//create variables to hold the humans's score and another to hold the computer's score
+let humanScore = 0;
+let computerScore = 0;
+
 //calculate computer's choice to randomnly select rock, paper, or scissors
 function getComputerChoice() {
   let randomNumber = Math.random();
