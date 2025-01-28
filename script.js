d79b366c795da3daccc6f92aee47030aa80ffb73
ianspractice <bodybody.ian@gmail.com>
2025-01-28 11:34:13 -0600

    Finish playRound function
    All possiblities accounted for.

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const log = console.log;
 //create variables to hold the humans's score and another to hold the computer's score
 let humanScore = 0;
 let computerScore = 0;
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
 
 //calculate computer's choice to randomnly select rock, paper, or scissors
 function getComputerChoice() {
@@ -22,13 +24,21 @@ function getHumanChoice() {
 
 //play a round and compare human choice to computer choice to see who wins
 function playRound(humanChoice, computerChoice) {
-  humanChoice = getHumanChoice();
-  computerChoice = getComputerChoice();
-  if (humanChoice === computerChoice) {
-    alert("same!");
+  if (humanChoice === "rock" && computerChoice === "paper") {
+    alert("Computer wins! Paper covers rock.");
+  } else if (humanChoice === "rock" && computerChoice === "scissors") {
+    alert("Human wins! Rock breaks scissors.");
+  } else if (humanChoice === "paper" && computerChoice === "rock") {
+    alert("Human wins! Paper covers rock.");
+  } else if (humanChoice === "paper" && computerChoice === "scissors") {
+    alert("Computer wins! Scissors cut paper.");
+  } else if (humanChoice === "scissors" && computerChoice === "paper") {
+    alert("Human wins! Scissors cut paper.");
+  } else if (humanChoice === "scissors" && computerChoice === "rock") {
+    alert("Computer wins! Rock breaks scissors.");
   } else {
-    alert("not same");
+    alert("Tie!");
   }
 }
-
-playRound();
+log("Human: " + humanSelection + " Computer: " + computerSelection);
+playRound(humanSelection, computerSelection);
